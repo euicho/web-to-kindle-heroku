78bf1743ecf5ed4f3b91b86b04b05562b2093ee0
Thomas Gagnon <euicho@gmail.com>
2022-02-27 21:58:45 -0500

    updated resolution for Paperwhite width: 1072, height: 1448

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ express()
   .get('/', async (req, res) => {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
-    await page.setViewport({ width: 600, height: 800 });
+    await page.setViewport({ width: 1072, height: 1448 });
     await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
     await page.screenshot({
       path: '/tmp/screenshot.png',
